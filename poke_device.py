8853670862b7fbde4d6515a17b2ed4494aac6d92
R. Ou <rqou@robertou.com>
2020-03-07 17:24:31 -0800

    Refactor program function

--- poke_device.py
+++ poke_device.py
@@ -100,6 +100,13 @@ class JTAGInteface:
         self.jtag_bit(1, 0)
         self.jtag_bit(0, 0)
 
+    def shift_dr_from_exit1(self):
+        print("exit1 -> shift dr")
+        self.jtag_bit(1, 0)
+        self.jtag_bit(1, 0)
+        self.jtag_bit(0, 0)
+        self._last_bit = self.jtag_bit(0, 0)
+
     def idcode(self):
         self.rti_from_tlr()
         self.shift_dr_from_rti()
@@ -107,6 +114,19 @@ class JTAGInteface:
         idcode = self.shift_bits([0] * 32, True)
         self.go_tlr()
         return arr2num(idcode)
+
+    def _xc2_discharge(self):
+        # DISCHARGE
+        self.shift_ir_from_rti()
+        self.shift_bits(num2arr(ISC_INIT, 8), True)
+        self.rti_from_exit1()
+        time.sleep(0.001)
+
+    def _xc2_init_pulse(self):
+        self.shift_ir_from_rti()
+        self.shift_bits(num2arr(ISC_INIT, 8), True)
+        self.init_pulse_from_exit1_to_rti()
+        time.sleep(0.001)
 
     def xc2_erase(self):
         self.rti_from_tlr()
@@ -120,16 +140,43 @@ class JTAGInteface:
         self.rti_from_exit1()
         time.sleep(0.1)
 
-        # DISCHARGE
+        self._xc2_discharge()
+        self._xc2_init_pulse()
+
         self.shift_ir_from_rti()
-        self.shift_bits(num2arr(ISC_INIT, 8), True)
+        self.shift_bits(num2arr(ISC_DISABLE, 8), True)
+        self.rti_from_exit1()
+
+        self.shift_ir_from_rti()
+        self.shift_bits(num2arr(BYPASS, 8), True)
+        self.go_tlr()
+
+    def xc2_program(self, crbit_bits):
+        self.rti_from_tlr()
+        self.shift_ir_from_rti()
+        self.shift_bits(num2arr(ISC_ENABLE, 8), True)
         self.rti_from_exit1()
         time.sleep(0.001)
 
         self.shift_ir_from_rti()
-        self.shift_bits(num2arr(ISC_INIT, 8), True)
-        self.init_pulse_from_exit1_to_rti()
-        time.sleep(0.001)
+        self.shift_bits(num2arr(ISC_PROGRAM, 8), True)
+        self.shift_dr_from_exit1()
+
+        for i in range(len(crbit_bits)):
+            # In shift-dr now
+            print("shifting row {}".format(i))
+            self.shift_bits(crbit_bits[i][::-1], False)
+            addr_gray = i ^ (i >> 1)
+            self.shift_bits(num2arr(addr_gray, 6)[::-1], True)
+            self.rti_from_exit1()
+            time.sleep(0.01)
+            if i != len(crbit_bits) - 1:
+                self.shift_dr_from_rti()
+
+        # In RTI
+
+        self._xc2_discharge()
+        self._xc2_init_pulse()
 
         self.shift_ir_from_rti()
         self.shift_bits(num2arr(ISC_DISABLE, 8), True)
@@ -147,14 +194,6 @@ class JTAGInteface:
 #     jtag_bit(dev, 1, 0)
 #     jtag_bit(dev, 0, 0)
 #     _last_bit = jtag_bit(dev, 0, 0)
-
-def shift_dr_from_exit1(dev):
-    global _last_bit
-    print("exit1 -> shift dr")
-    jtag_bit(dev, 1, 0)
-    jtag_bit(dev, 1, 0)
-    jtag_bit(dev, 0, 0)
-    _last_bit = jtag_bit(dev, 0, 0)
 
 dev = JTAGInteface()
 idcode = dev.idcode()
@@ -227,63 +266,23 @@ print("idcode is 0x{:08X}".format(idcode))
 
 dev.xc2_erase()
 
-# ##### PROGRAM #####
-# crbit_bits = []
-# with open("aaa.crbit", "r") as f:
-#     for l in f.readlines():
-#         l = l.strip()
-#         if not l:
-#             continue
-#         if l.startswith("//"):
-#             continue
-#         # print(l)
-#         linebits = [1 if c == '1' else 0 for c in l]
-#         # print(linebits)
-#         assert len(linebits) == 260
-#         crbit_bits.append(linebits)
+##### PROGRAM #####
+crbit_bits = []
+with open("aaa.crbit", "r") as f:
+    for l in f.readlines():
+        l = l.strip()
+        if not l:
+            continue
+        if l.startswith("//"):
+            continue
+        # print(l)
+        linebits = [1 if c == '1' else 0 for c in l]
+        # print(linebits)
+        assert len(linebits) == 260
+        crbit_bits.append(linebits)
 
-# assert len(crbit_bits) == 50
-# print(crbit_bits)
-# # aaaaaaaaaaaaa
+assert len(crbit_bits) == 50
+print(crbit_bits)
+# aaaaaaaaaaaaa
 
-# rti_from_tlr(dev)
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(ISC_ENABLE, 8), True)
-# rti_from_exit1(dev)
-# time.sleep(0.001)
-
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(ISC_PROGRAM, 8), True)
-# shift_dr_from_exit1(dev)
-
-# for i in range(len(crbit_bits)):
-#     # In shift-dr now
-#     print("shifting row {}".format(i))
-#     shift_bits(dev, crbit_bits[i][::-1], False)
-#     addr_gray = i ^ (i >> 1)
-#     shift_bits(dev, num2arr(addr_gray, 6)[::-1], True)
-#     rti_from_exit1(dev)
-#     time.sleep(0.01)
-#     if i != len(crbit_bits) - 1:
-#         shift_dr_from_rti(dev)
-
-# # In RTI
-
-# # DISCHARGE
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(ISC_INIT, 8), True)
-# rti_from_exit1(dev)
-# time.sleep(0.001)
-
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(ISC_INIT, 8), True)
-# init_pulse_from_exit1_to_rti(dev)
-# time.sleep(0.001)
-
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(ISC_DISABLE, 8), True)
-# rti_from_exit1(dev)
-
-# shift_ir_from_rti(dev)
-# shift_bits(dev, num2arr(BYPASS, 8), True)
-# go_tlr(dev)
+dev.xc2_program(crbit_bits)
